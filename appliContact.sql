513638f2ec3e2ef046ede395ef90a57576726cfa
NHerve <herve.04@hotmail.fr>
2017-03-08 13:12:50 +0100

    Add files via upload

---
+++ appliContact.sql
@@ -0,0 +1,11 @@
+
+create database dbContact;
+
+grant all privileges on dbContact.* to adminDBContact identified by '@42Bz8ai';
+
+
+create table contact (	idContact integer not null primary key,
+			prenom char(255) not null,
+			nom char(255) not null,
+			mail char(255),
+			numPortable char(255) not null);
